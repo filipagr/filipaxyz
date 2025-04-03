cae0364ced411855e20b95416ffd83f4dd738285
filipagr <filipagoncalvesribeiro@gmail.com>
2025-04-03 18:39:35 +0100

    Update page.jsx

--- src/app/projects/[slug]/page.jsx
+++ src/app/projects/[slug]/page.jsx
@@ -5,15 +5,11 @@ export async function generateStaticParams() {
   const projectDir = path.join(process.cwd(), 'src/app/projects')
   const files = fs.readdirSync(projectDir)
 
-  const slugs = files
+  return files
     .filter((file) => fs.statSync(path.join(projectDir, file)).isDirectory())
     .map((folderName) => ({
       slug: folderName,
     }))
-
-  console.log('📦 Generating static params for project slugs:', slugs)
-
-  return slugs
 }
 
 export default function ProjectPage() {
